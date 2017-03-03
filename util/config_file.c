d8742507abc3a809f9961b0fd36328348d7cd282
chantra <chantr4@gmail.com>
2017-03-03 11:47:01 -0800

    [dnscrypt] set dnscrypt disabled by default and suport dnscrypt diabled while dnscrypt options are specified.
    When dnscrypt_enabled is set to no, we should not enable dnscrypt on
    dnscrypt_port. In order to do so, dnscrypt_port will be set to 0 when
    dnscrypt_enable is false. This will prevent matching the interface in
    listen_dnsport.c's ports_create_if

--- util/config_file.c
+++ util/config_file.c
@@ -259,6 +259,7 @@ config_create(void)
 	cfg->qname_minimisation_strict = 0;
 	cfg->shm_enable = 0;
 	cfg->shm_key = 11777;
+    cfg->dnscrypt = 0;
 	cfg->dnscrypt_port = 0;
 	cfg->dnscrypt_provider = NULL;
 	cfg->dnscrypt_provider_cert = NULL;
@@ -943,6 +944,8 @@ config_read(struct config_file* cfg, const char* filename, const char* chroot)
 	ub_c_in = in;
 	ub_c_parse();
 	fclose(in);
+
+    if(!cfg->dnscrypt) cfg->dnscrypt_port = 0;
 
 	if(cfg_parser->errors != 0) {
 		fprintf(stderr, "read %s failed: %d errors in configuration file\n",
